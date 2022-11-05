95201fbb086df59844f446b23193e108331c7056
Thomas Ghignon <thomas@everide.app>
2022-11-05 12:34:54 +0100

    🚀[UPGRADE] Matterjs is now working + add some colors and effects on bubbles. Bubbles are clickables

--- assets/controllers/matter.js
+++ assets/controllers/matter.js
@@ -3,104 +3,93 @@ import * as Matter from 'matter-js';
 // module aliases
 var Engine = Matter.Engine,
     Render = Matter.Render,
-    Runner = Matter.Runner,
-    Bodies = Matter.Bodies,
-    Composite = Matter.Composite;
+    World = Matter.World,
+    Body = Matter.Body,
+    Bodies = Matter.Bodies;
 
-// create an engine
-var engine = Engine.create();
+var worldW = window.innerWidth;
+var worldH = window.innerHeight;
 
-var render = Render.create({
-    element: document.body,
-    engine: engine,
-    options: {
-        wireframes: false,
-        background: 'transparent',
+
+var engine;
+var render;
+
+// bodies
+var blocks = [];
+var walls = [];
+var ground;
+var ceiling;
+var wallRight;
+var wallLeft;
+
+// DOM elements
+var hBlocks = document.getElementsByClassName("bubble");
+
+function Ball(x,y,r){
+    var options = {
+        density: 1,
+        friction: 100,
+        restitution: 0,
+        render: {
+            fillStyle: 'transparent',
+        }
     }
-});
-
-render.canvas.width = window.innerWidth;
-render.canvas.height = window.innerHeight;
-
-Render.run(render);
-
-const placement = {x: 1, y: 1};
-const spacing = {x: 300, y: 300};
-
-var ground = Bodies.rectangle(window.innerWidth/2, window.innerHeight, window.innerWidth, 10,
-    {
-        isStatic: true,
-        render: {
-            fillStyle: 'transparent',
-        }
-    });
-var wallRight = Bodies.rectangle(0, window.innerHeight/2, 1, window.innerHeight,
-    {
-        isStatic: true,
-        render: {
-            fillStyle: 'transparent',
-        }
-    });
-var wallLeft = Bodies.rectangle(window.innerWidth, window.innerHeight/2, 0, window.innerHeight,
-    {
-        isStatic: true,
-        render: {
-            fillStyle: 'transparent',
-        }
-    });
-
-function addObjects () {
-
-    var tab = [];
-    document.getElementById('home').querySelectorAll('[data-object]').forEach((object, index) => {
-        tab.push(fillObject(object, index));
-    });
-
-    tab.push(ground, wallRight, wallLeft);
-
-    return tab;
+    this.body = Bodies.circle(x, y, r, options);
+    World.add(engine.world, [this.body]);
 }
 
-function fillObject(object, index) {
-    var data = object.getAttribute('data-object').split('|')
-    console.log(data)
-    return Bodies.circle(
-        window.innerWidth/2,
-        index,
-        80, {
-            friction: 100,
-            label: data[2],
-            render: {
-                fillStyle: '#e74c3c',
-                strokeStyle: "#fff",
-                lineWidth: 0,
-                text: {
-                    fillStyle: "#000000",
-                    content: "test",
-                    size: 50,
-                },
-            }
-        })
-}
+function setup(){
+    engine = Engine.create();
 
-var boxB = Bodies.circle(
-    window.innerWidth/2,
-    10,
-    80, {
-        friction: 0.5,
-        render: {
-            fillStyle: '#e74c3c',
-            strokeStyle: "#fff",
-            lineWidth: 0
+    render = Render.create({
+        element: document.body,
+        engine: engine,
+        options: {
+            width: window.innerWidth,
+            height: window.innerHeight,
+            wireframes: false,
+            showAngleIndicator:false,
+            background: 'transparent',
         }
     });
+    Render.run(render);
+    for(var i=0;i<hBlocks.length;i++){
+        blocks.push(new Ball(window.innerWidth/2, 0, 100));
+    }
+    ground = Bodies.rectangle(window.innerWidth/2, window.innerHeight, window.innerWidth, 1, {
+        isStatic: true,
+        render: {
+            fillStyle: 'transparent',
+        }
+    });
+    wallRight = Bodies.rectangle(0, window.innerHeight/2, 1, window.innerHeight, {
+        isStatic: true,
+        render: {
+            fillStyle: 'transparent',
+        }
+    });
+    wallLeft = Bodies.rectangle(window.innerWidth, window.innerHeight/2, 1, window.innerHeight, {
+        isStatic: true,
+        render: {
+            fillStyle: 'transparent',
+        }
+    });
+}
 
-Composite.add(engine.world, addObjects());
+function draw(){
+    World.add(engine.world, [ground, wallLeft, wallRight]);
+}
 
+setup();
+draw();
 
-
-
-Render.run(render);
-
-var runner = Runner.create();
-Runner.run(runner, engine);
+(function render() {
+    Engine.update(engine, 20);
+    for (let i = 0; i < blocks.length; i++) {
+        var xTrans = blocks[i].body.position.x - hBlocks[i].offsetWidth/2;
+        var yTrans = blocks[i].body.position.y - hBlocks[i].offsetHeight/2;
+        hBlocks[i].style.transform = "translate("+xTrans+"px, "+yTrans+"px) rotate("+blocks[i].body.angle+"rad)";
+        hBlocks[i].style.visibility = "visible";
+    }
+    window.requestAnimationFrame(render);
+})();
