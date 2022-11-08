c37f051efd30e1811d672bf92e060c2b77c8bbda
Thomas Ghignon <thomas@everide.app>
2022-11-08 14:59:20 +0100

    🚀[UPGRADE] user register and login avaible

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@ module.exports = {
     content: [
         "./assets/**/*.js",
         "./templates/**/*.html.twig",
-        // "./node_modules/flowbite/**/*.js",
     ],
     theme: {
         extend: {
